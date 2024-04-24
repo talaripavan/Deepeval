06a5ec5c7a2d535d9c89fa33cfd6f4c61ccb2db5
Talari Pavan Kumar <95948550+talaripavan@users.noreply.github.com>
2024-04-24 19:53:54 +0530

    Code with expected output for RAG Evaluation

--- test_deepeval.py
+++ test_deepeval.py
@@ -17,10 +17,10 @@ answer_relevancy = AnswerRelevancyMetric(threshold=0.5)
 faithfulness = FaithfulnessMetric(threshold=0.5)
 evaluation_metrics = [
   bias,
-  #contextual_precision,
-  #contextual_recall,
+  contextual_precision,
+  contextual_recall,
   answer_relevancy,
-  #faithfulness
+  faithfulness
 ]
 
 from dotenv import load_dotenv
@@ -51,8 +51,33 @@ index = VectorStoreIndex.from_documents(documents, storage_context=storage_conte
 llm = OpenAI(model="gpt-3.5-turbo")
 rag_application = index.as_query_engine(llm=llm)
 
+from llama_index.core import PromptTemplate
+
+text_qa_template_str = (
+    "Context information is"
+    " below.\n---------------------\n{context_str}\n---------------------\nUsing"
+    " both the context information and also using your own knowledge, answer"
+    " the question: {query_str}\nIf the context isn't helpful, you can also"
+    " answer the question on your own.\n"
+)
+text_qa_template = PromptTemplate(text_qa_template_str)
+
+refine_template_str = (
+    "The original question is as follows: {query_str}\nWe have provided an"
+    " existing answer: {existing_answer}\nWe have the opportunity to refine"
+    " the existing answer (only if needed) with some more context"
+    " below.\n------------\n{context_msg}\n------------\nUsing both the new"
+    " context and your own knowledge, update or repeat the existing answer.\n"
+)
+refine_template = PromptTemplate(refine_template_str)
+
+prompt_output = index.as_query_engine(
+        text_qa_template=text_qa_template,
+        refine_template=refine_template,
+        llm=llm,
+    )
     
-file_path = "deepEval_datasets/evaluation_data/test1.json"
+file_path = "deepEval_datasets/evaluation_data/20240424_175658.json"
 with open(file_path, "r") as json_file:
     json_data = json.load(json_file)
     
@@ -62,14 +87,18 @@ with open(file_path, "r") as json_file:
 )
 def test_llamaindex(input_output_pair: Dict):
     input = input_output_pair.get("input", None)
-    expected_output = input_output_pair.get("expected_output", None)
     response_object = rag_application.query(input)
     
     if response_object is not None:
         actual_output = response_object.response
-        retrieval_context = [node.get_content() for node in response_object.source_nodes]
+        retrieval_context = [node.get_content() for node in response_object.source_nodes]  
     actual_output = actual_output
     retrieval_context = retrieval_context
+    
+    expected =prompt_output.query(input)
+    if expected is not None:
+        expected_output = expected.response
+    expected_output=expected_output
     
     test_case = LLMTestCase(
         input=input,
@@ -79,8 +108,8 @@ def test_llamaindex(input_output_pair: Dict):
     )
     assert_test(test_case, evaluation_metrics)
 
-import deepeval
 
+import deepeval
 @deepeval.on_test_run_end
 def function_to_be_called_after_test_run():
     directory_path = os.environ['DEEPEVAL_RESULTS_FOLDER']
@@ -89,3 +118,6 @@ def function_to_be_called_after_test_run():
     json_file_path = deepeval_db.rename_file_to_json(directory_path)
     deepeval_db.insert_data(json_file_path)
     print("Test finished Pavan")
+
+
+
